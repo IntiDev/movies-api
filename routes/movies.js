53f067ea71651a20980dd99b6fe054373c4b1864
Inti <yeseniase@outlook.it>
2019-10-25 17:47:56 -0500

    ADD: schemas de validaciones con JOI

--- routes/movies.js
+++ routes/movies.js
@@ -1,84 +1,111 @@
 const express = require('express');
 const MoviesService = require('../services/movies');
 
+const {
+  movieIdSchema,
+  createMovieSchema,
+  updateMovieSchema
+} = require('../utils/schemas/movies');
+const validationHandler = require('../utils/middleware/validationHandler');
+
 const moviesApi = app => {
-    const router = express.Router();
-    app.use('/api/movies', router);
+  const router = express.Router();
+  app.use('/api/movies', router);
 
-    const moviesService = new MoviesService();
+  const moviesService = new MoviesService();
 
-    router.get('/', async (req, res, next) => {
-        const { tags } = req.query;//Un query es cuando se pone el signo de ? el nombre del query y se puede concatenar.
+  router.get('/', async (req, res, next) => {
+    const { tags } = req.query; //Un query es cuando se pone el signo de ? el nombre del query y se puede concatenar.
 
-        try {
-            const movies = await moviesService.getMovies({ tags });
-            throw new Error('Error getting movies');
-            
-            res.status(200).json({
-                data: movies,
-                message: 'movies listed'
-            });
-        } catch(err) {
-            next(err);
-        }
-    });
+    try {
+      const movies = await moviesService.getMovies({ tags });
+      throw new Error('Error getting movies');
 
-    router.get('/:movieId', async (req, res, next) => {
-        const { movieId } = req.params;//La diferencia con el query es que los parametros están establecidos en la url.
+      res.status(200).json({
+        data: movies,
+        message: 'movies listed'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 
-        try {
-            const movies = await moviesService.getMovies({ movieId })
-            res.status(200).json({
-                data: movies,
-                message: 'movies retrieve'
-            });
-        } catch(err) {
-            next(err);
-        }
-    });
+  router.get(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    async (req, res, next) => {
+      const { movieId } = req.params; //La diferencia con el query es que los parametros están establecidos en la url.
 
-    router.post('/', async (req, res, next) => {
-        const { body: movie } = req;
+      try {
+        const movies = await moviesService.getMovies({ movieId });
+        res.status(200).json({
+          data: movies,
+          message: 'movies retrieve'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
-        try {
-            const createMovieId = await moviesService.createMovie({ movie });
-            res.status(201).json({
-                data: createMovieId,
-                message: 'movie created'
-            });
-        } catch(err) {
-            next(err);
-        }
-    })
+  router.post(
+    '/',
+    validationHandler(createMovieSchema),
+    async (req, res, next) => {
+      const { body: movie } = req;
 
-    router.put('/:movieId', async (req, res, next) => {
-        const { movieId } = req.params;
-        const { body: movie } = req;
+      try {
+        const createMovieId = await moviesService.createMovie({ movie });
+        res.status(201).json({
+          data: createMovieId,
+          message: 'movie created'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
-        try {
-            const updatedMovieId = await moviesService.updateMovie({ movieId, movie });
-            res.status(200).json({
-                data: updatedMovieId,
-                message: 'movie updated'
-            });
-        } catch(err) {
-            next(err);
-        }
-    })
+  router.put(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    validationHandler(updateMovieSchema),
+    async (req, res, next) => {
+      const { movieId } = req.params;
+      const { body: movie } = req;
 
-    router.delete('/:movieId', async (req, res, next) => {
-        const { movieId } = req.params;
+      try {
+        const updatedMovieId = await moviesService.updateMovie({
+          movieId,
+          movie
+        });
+        res.status(200).json({
+          data: updatedMovieId,
+          message: 'movie updated'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
-        try {
-            const deleteMovieId = await moviesService.deletedMovie({ movieId });
-            res.status(200).json({
-                data: deleteMovieId,
-                message: 'movie deleted'
-            });
-        } catch(err) {
-            next(err);
-        }
-    })
+  router.delete(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    async (req, res, next) => {
+      const { movieId } = req.params;
+
+      try {
+        const deleteMovieId = await moviesService.deletedMovie({ movieId });
+        res.status(200).json({
+          data: deleteMovieId,
+          message: 'movie deleted'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 };
 
 module.exports = moviesApi;
